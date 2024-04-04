683a1dc6346637ed4ca2b2aaa178f844ed063ea8
Jason Esterhuizen <jayfalls@users.noreply.github.com>
2024-04-04 11:11:02 +0200

    [REFACT] Added printouts

--- app/components/model_provider/api.py
+++ app/components/model_provider/api.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
 ## Local
+from helpers import debug_print
+from constants.settings import DebugLevels
 from .provider import generate_response
 
 
@@ -20,5 +22,7 @@ api = FastAPI()
 # ROUTES
 @api.get("/generate", response_class=JSONResponse)
 async def generate(prompt: Prompt) -> dict:
+    print(f"Generating response for {prompt.stack_type}...")
     response: str = generate_response(stack_type=prompt.stack_type, system_prompt=prompt.system_prompt)
+    debug_print(f"Response: {response}", debug_level=DebugLevels.INFO)
     return {"response": response}
